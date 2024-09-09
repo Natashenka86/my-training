cf855742257323357c9675bc4771ae0ff7d45b01
Natashenka86 <natalyponomarenko0@gmail.com>
2024-09-09 10:37:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+name= "Nata"
+print ("hello"+name)
+print ('hello'*5)
+print (name [0])
+print (name [0:3])
+example="Flower"
+print (example [0])
+print (example [-1])
+print (example [3:6])
+print (example [1:6:2])
+print (example [::-1])
